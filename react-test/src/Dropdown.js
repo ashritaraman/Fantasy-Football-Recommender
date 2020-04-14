74b6001eea54b777bbed41c466962ebe226a225a
Aparna Calambur <a.calambur@gmail.com>
2020-04-13 21:06:36 -0400

    sjfklsd

--- react-test/src/Dropdown.js
+++ react-test/src/Dropdown.js
@@ -1,20 +1,19 @@
 import _ from 'lodash'
 import React from 'react'
 import { Dropdown } from 'semantic-ui-react'
+import Players from './players.json'
 
-const players = [{ key: 'Petr', value: 'Cech', text: 'Petr Cech' },
-{ key: 'Bernd', value: 'Leno', text: 'Bernd Leno' },
-{ key: 'Laurent', value: 'Koscielny', text: 'Laurent Koscielny' },]
+
+const play = Players
 
 const DropdownExampleMultipleSearchSelection = () => (
   <Dropdown
     placeholder='Player'
-    label='Player'
     fluid
     multiple
     search
     selection
-    options={players}
+    options={play}
   />
 )
 
