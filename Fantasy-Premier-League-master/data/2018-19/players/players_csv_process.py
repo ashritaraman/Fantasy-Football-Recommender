f8e3a32e803368a28fac09ab6e717f70b84afd7b
Ashrita Raman <ar699@cornell.edu>
2020-05-05 16:54:42 -0400

    nn.py

--- Fantasy-Premier-League-master/data/2018-19/players/players_csv_process.py
+++ Fantasy-Premier-League-master/data/2018-19/players/players_csv_process.py
@@ -54,7 +54,6 @@ def make_big_csv(gw):
             for row in gw_df_list:
                 if row[10] == gw:
                     final_total_points = row[11]
-                
                 if row[10] == gw-5:
                     lst_5 = row
                 if row[10] == gw-4:
@@ -67,16 +66,15 @@ def make_big_csv(gw):
                     lst_1 = row
 
             temp_lst = [] 
-            # print(gw)
-            # print(folder_name)
             i = 0
-            while i<len(lst_1):
+            while i<len(lst_1): #all list sizes should be the same--12
                 big_row = lst_1[i]*0.4 + lst_2[i]*0.3 + lst_3[i]*0.15 + lst_4[i]*0.1 + lst_5[i]*0.05
                 temp_lst.append(big_row)
                 i=i+1
 
             temp_lst.append(player_pos)    
             temp_lst.append(game_week)
+            temp_lst.append(str(elem[3]))
             temp_lst.append(final_total_points)
             df_list.append(temp_lst)
             
@@ -85,7 +83,7 @@ def make_big_csv(gw):
     return df_list
 
 
-cols = ['assists','big_chances_created','clean_sheets','clearances_blocks_interceptions','dribbles','errors_leading_to_goal','ict_index','key_passes','penalties_missed','penalties_saved','round','total_points_prev','player_pos','gw','current_gw_points']
+cols = ['assists','big_chances_created','clean_sheets','clearances_blocks_interceptions','dribbles','errors_leading_to_goal','ict_index','key_passes','penalties_missed','penalties_saved','round','player_pos','gw','current_gw_points', 'player_id', 'total_points_prev']
 df_list = make_big_csv(6)
 
 df_fwd= pd.DataFrame(df_list, columns = cols)
