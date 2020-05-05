934235a500a65e719a44d265119c6634d8408e6d
Param Mahajan <psm222@cornell.edu>
2020-05-05 17:54:05 -0400

    citations added

--- Fantasy-Premier-League-master/data/2018-19/players/nn.py
+++ Fantasy-Premier-League-master/data/2018-19/players/nn.py
@@ -1,3 +1,6 @@
+# https://www.freecodecamp.org/news/how-to-build-your-first-neural-network-to-predict-house-prices-with-keras-f8db83049159/
+# https://machinelearningmastery.com/regression-tutorial-keras-deep-learning-library-python/
+
 import pandas as pd
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
