af956ae23bd1bda476d59c27283dd1ad20d66ce7
Param Mahajan <psm222@cornell.edu>
2020-05-06 13:06:33 -0400

    model 2

---
+++ Fantasy-Premier-League-master/data/2018-19/players/nn_regression.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Dense
+
+# https://www.freecodecamp.org/news/how-to-build-your-first-neural-network-to-predict-house-prices-with-keras-f8db83049159/
+# https://machinelearningmastery.com/regression-tutorial-keras-deep-learning-library-python/
+
