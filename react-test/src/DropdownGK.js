e5420dde68f258ca325f6819afbbcce498a239af
Aparna Calambur <a.calambur@gmail.com>
2020-05-06 22:01:13 -0400

    form updates

--- react-test/src/DropdownGK.js
+++ react-test/src/DropdownGK.js
@@ -1,20 +1,38 @@
 import _ from 'lodash'
 import React from 'react'
+import { Component } from "react";
 import { Dropdown } from 'semantic-ui-react'
 import Players from './player_gk.json'
 
 
 const play = Players
 
-const DropdownExampleMultipleSearchSelection = () => (
-  <Dropdown
-    placeholder='Player'
-    fluid
-    multiple
-    search
-    selection
-    options={play}
-  />
-)
+class DropdownExampleMultipleSearchSelection extends Component {
 
-export default DropdownExampleMultipleSearchSelection
+  state = {
+    defender: [],
+    fwd: [],
+    list: [],
+  }
+
+  handleChange = (event, data) => {
+    console.log(data.value);
+  }
+
+  render() {
+    return (
+      <Dropdown
+        placeholder='Player'
+        fluid
+        search
+        selection
+        options={play}
+        onChange={this.handleChange}
+      />
+
+    );
+  }
+
+}
+
+export default DropdownExampleMultipleSearchSelection;
--- react-test/src/FormEx.js
+++ react-test/src/FormEx.js
@@ -1,56 +1,124 @@
 import React, { Component } from "react";
 import { Form } from "semantic-ui-react";
-import Dropdown from "./Dropdown.js";
 import DropdownGK from "./DropdownGK.js";
 import DropdownDEF from "./DropdownDEF.js";
 import DropdownMID from "./DropdownMID.js";
 import DropdownFWD from "./DropdownFWD.js";
 import { Container, Divider, Grid, Header, Image } from "semantic-ui-react";
 import { Segment } from "semantic-ui-react";
+import { Button, Modal } from "semantic-ui-react";
+import { Dropdown } from 'semantic-ui-react'
+import Players from './player_gk.json'
 
 class FormExampleSubcomponentControl extends Component {
-  state = {};
+  state = {
+    goalkeeper1: "",
+    goalkeeper2: "",
+    styles: {
+      position: "relative",
+    },
+  }
 
-  handleChange = (e, { value }) => this.setState({ value });
+
+  // onUpdateItems = (e, index) => {
+  //   const list = this.state.list;
+  //   //list[index] = e.target.value;
+  //   this.setState({
+  //     list
+  //   });
+  // };
+  // onUpdateItems = (event) => {
+  //   // this.setState(state => {
+  //   //   const list = state.list.map(item => item + 1);
+  //   //   return {
+  //   //     list,
+  //   //   };
+  //   // });
+
+  // };
+
+
+  handleChange1 = (event, data) => {
+    this.setState({ goalkeeper1: data.value });
+  }
+  handleChange2 = (event, data) => {
+    this.setState({ goalkeeper2: data.value });
+  }
+
 
   render() {
     const { value } = this.state;
     return (
-      <Segment basic padded="very">
-        <Header as="h2" textAlign="center">
-          Team Selection Form
+      <Container>
+        <Segment basic padded="very">
+          <Header as="h2" textAlign="center">
+            Team Selection Form
         </Header>
-        <Form>
-          <Grid columns={2} padded>
-            <Grid.Column>
-              <Form.Field label="Choose 1 Captain" control={Dropdown} />
-            </Grid.Column>
-            <Grid.Column>
-              <Form.Field label="Choose 1 Vice Captain" control={Dropdown} />
-            </Grid.Column>
-            <Grid.Column>
-              <Form.Field label="Choose 2 Goal Keepers" control={DropdownGK} />
-            </Grid.Column>
-            <Grid.Column>
-              <Form.Field label="Choose 4 Defense" control={DropdownDEF} />
-            </Grid.Column>
-            <Grid.Column>
-              <Form.Field label="Choose 4 Midfielders" control={DropdownMID} />
-            </Grid.Column>
-            <Grid.Column>
-              <Form.Field label="Choose 3 Forwards" control={DropdownFWD} />
-            </Grid.Column>
+          <Form>
+            <Grid columns={2} padded>
+              <Grid.Column>
+                <Form.Field label="Choose Goal Keeper 1" />
+                <Dropdown
+                  placeholder='Player'
+                  fluid
+                  search
+                  selection
+                  options={Players}
+                  onChange={this.handleChange1}
+                />
+                <Form.Field label="Choose Goal Keeper 2" />
+                <Dropdown
+                  placeholder='Player'
+                  fluid
+                  search
+                  selection
+                  options={Players}
+                  onChange={this.handleChange2}
+                />
+              </Grid.Column>
+              <Grid.Column>
+                <Form.Field label="Choose 4 Defense" control={DropdownDEF} />
+              </Grid.Column>
+              <Grid.Column>
+                <Form.Field label="Choose 4 Midfielders" control={DropdownMID} />
+              </Grid.Column>
+              <Grid.Column>
+                <Form.Field label="Choose 3 Forwards" control={DropdownFWD} />
+              </Grid.Column>
 
-            <div>
-              {
-                //<Form.Button>Submit</Form.Button>
-              }
-            </div>
-          </Grid>
-        </Form>
-      </Segment>
+              <div>
+                {
+                  //<Form.Button>Submit</Form.Button>
+                }
+              </div>
+            </Grid>
+          </Form>
+        </Segment>
+        <Segment basic>
+          <Modal
+            trigger={
+              <div id="button">
+                <Button style={this.state.styles} color="blue">
+                  Submit
+        </Button>
+              </div>
+            }>
+
+            <Modal.Header>Select a Photo</Modal.Header>
+            <Modal.Content image>
+              <Image wrapped size="medium" src="/images/avatar/large/rachel.png" />
+              <Modal.Description>
+                <Header>Score of the team you picked </Header>
+                <p>{this.state.goalkeeper1}</p>
+                <p>{this.state.goalkeeper2}</p>
+              </Modal.Description>
+            </Modal.Content>
+          </Modal>
+        </Segment>
+      </Container>
     );
   }
 }
 
 export default FormExampleSubcomponentControl;
+
